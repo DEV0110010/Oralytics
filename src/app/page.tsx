26f875f3f0f9b017b45b34091771e18d3f2d38b4
DEV0110010 <newera.deepanshu@gmail.com>
2025-10-06 12:40:25 +0530

    cta landing pages done

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,18 +1,23 @@
+import CTA from '@/components/landing/CTA'
+import Footer from '@/components/landing/Footer'
+import Header from '@/components/landing/Header'
+import Hero from '@/components/landing/Hero'
+import HowItWorks from '@/components/landing/HowItWorks'
+import PricingSection from '@/components/landing/PricingSection'
+import WhatToAsk from '@/components/landing/WhatToAsk'
 import { SignedIn, SignedOut, SignOutButton, SignUpButton } from '@clerk/nextjs'
 
 const page = () => {
   return (
-    <div>
-     <SignedOut>
-       <SignUpButton mode='modal'>
-        Sign Up
-      </SignUpButton>
-     </SignedOut>
-     <SignedIn>
-      <SignOutButton>
-        LogOut
-      </SignOutButton>
-     </SignedIn>
+    <div className='min-hscreen bg-background'>
+      <Header/>
+      <Hero/>
+      <HowItWorks/>
+      <WhatToAsk/>
+      <PricingSection/>
+      <CTA/>
+      <Footer/>
+
     </div>
   )
 }
